0082a1aab423b0b0f2ef1c7300ab04824008f3f5
jonasgoossensPXL <jonas.goossens2@student.pxl.be>
2022-04-01 16:20:19 +0200

    Admin Page added

--- Front-end/src/router/index.js
+++ Front-end/src/router/index.js
@@ -4,6 +4,7 @@ import OverOns from '../views/OverOns.vue'
 import Realisaties from '../views/Realisaties.vue'
 import Contact from '../views/Contact.vue'
 import Productinformatie from '@/views/Productinformatie.vue'
+import Admin from '@/views/Admin.vue'
 
 const routes = [
   {
@@ -30,6 +31,11 @@ const routes = [
     path: '/Productinformatie',
     name: 'Productinformatie',
     component: Productinformatie
+  },
+  {
+    path: '/Admin',
+    name: 'Admin',
+    component: Admin
   }
 ]
 
---
+++ Front-end/src/views/Admin.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        Foto toevoegen:
+    </div>
+    <input type="file">
+    <button v-on:click="submit"></button>
+</template>
+<script>
+export default{
+    data(){
+        return{
+            imageLocatie: ''
+        }
+    },
+    methods:{
+        submit: function (){
+            this.image
+        },
+    }
+}
+</script>
+<style>
+</style>
